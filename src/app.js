68ab53ee7eddfd78edf9e8b3af58e67d414d069f
Choxmi <gwcsathsara@gmail.com>
2021-09-25 00:14:02 +0530

    Global context

--- src/app.js
+++ src/app.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route,Switch,BrowserRouter } from "react-router-dom";
+import GlobalContext from './global';
 import ScreeningRecordInput from './routes/screeningRecordInput';
+import Translations from "./translations.json";
 
 function App() {
+    const [dark,setDark] = useState(window.localStorage.getItem("theme") === "dark");
+    const [locale,setLocale] = useState(window.localStorage.getItem("locale") !== null && window.localStorage.getItem("locale") !== undefined ? window.localStorage.getItem("locale") : 0);
+    const toggleTheme = ()=> {
+        window.localStorage.setItem("theme",!dark?"dark":"light");
+        setDark(!dark);
+        window.location.reload();
+    };
+    const [expanded,setExpanded] = useState(false);
+    let global = {
+        theme: dark,
+        toggleTheme: toggleTheme,
+        locale: locale,
+        setLocale: setLocale,
+        expanded: expanded,
+        setExpanded: setExpanded,
+        primary_color: dark ? 'primary-dark' : 'primary-light',
+        primary_color_background: dark ? 'primary-dark-background' : 'primary-light-background',
+        background_color: dark ? 'background-dark' : 'background-light',
+        primary_text_color: dark ? 'primary-text-dark' : 'primary-text-light',
+        secondary_text_color: dark ? 'secondary-text-dark' : 'secondary-text-light',
+        sub_text_color: dark ? 'sub-text-dark' : 'sub-text-light',
+        card_color: dark ? 'card-dark' : 'card-light',
+        text: Translations
+    }
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-            <ScreeningRecordInput/>
-        </Route>
-        <Route path="*">
-            <ScreeningRecordInput/>
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <GlobalContext.Provider value={global}>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <ScreeningRecordInput/>
+                </Route>
+                <Route path="*">
+                    <ScreeningRecordInput/>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    </GlobalContext.Provider>
   );
 }
 
--- src/routes/screeningRecordInput.js
+++ src/routes/screeningRecordInput.js
@@ -1,10 +1,45 @@
+import { useContext } from "react";
 import BrowserWindow from "../browserWindow";
 import Card from "../components/card/card";
+import GlobalContext from "../global";
 
 const ScreeningRecordInput = () => {
+
+    const { locale, text } = useContext(GlobalContext);
+
     return <BrowserWindow>
         <Card>
-            <h1>TESTTTTT</h1>
+            <table>
+                <tr><th colSpan={4}>{text.screening_record.step1.title[locale]}</th></tr>
+                <tr>
+                    <td>{text.screening_record.step1.card[locale]}</td>
+                    <td colSpan={3}></td>
+                </tr>
+                <tr>
+                    <td>{text.screening_record.step1.name[locale]}</td>
+                    <td colSpan={3}></td>
+                </tr>
+                <tr>
+                    <td>{text.screening_record.step1.dob[locale]}</td>
+                    <td></td>
+                    <td>{text.screening_record.step1.age[locale]}</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>{text.screening_record.step1.sex[locale]}</td>
+                    <td></td>
+                    <td>{text.screening_record.step1.contact[locale]}</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>{text.screening_record.step1.address[locale]}</td>
+                    <td colSpan={3}></td>
+                </tr>
+                <tr>
+                    <td>{text.screening_record.step1.past[locale]}</td>
+                    <td colSpan={3}></td>
+                </tr>
+            </table>
         </Card>
     </BrowserWindow>
 }
